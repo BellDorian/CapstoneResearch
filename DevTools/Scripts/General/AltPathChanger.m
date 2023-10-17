210b82beafa2a754e424e0d31c7c3b757dced8d7
tkolat <tkolat@NZXT.localdomain>
2023-10-17 19:31:40 -0400

    updated

--- DevTools/Scripts/General/AltPathChanger.m
+++ DevTools/Scripts/General/AltPathChanger.m
@@ -1,9 +1,15 @@
 % Load the gTruth object
-load('side1_03_new.mat', 'gTruth');
+load('Side2_03.mat', 'gTruth');
 
 % Define the old (current) and new (alternative) paths
-currentPath = 'C:\Users\MsDor\OneDrive\Documents\MATLAB\side1_3.mp4';
-alternativePath = 'D:\myCode\CAPSTONE\Videos\side1\side1_3.mp4';
+currentPath = 'D:\myCode\CAPSTONE\A2_Videos\Side2\Side2_03.mp4';
+alternativePath = 'D:\myCode\CAPSTONE\A2_Videos\Side2\side2_3.mp4';
+
+if exist(alternativePath, 'file')
+    disp('File exists.');
+else
+    disp('File does not exist.');
+end
 
 % Create a cell array with the current and alternative paths
 pathPairs = {currentPath, alternativePath};
@@ -11,10 +17,12 @@ pathPairs = {currentPath, alternativePath};
 % Use changeFilePaths to update the paths in gTruth
 unresolvedPaths = changeFilePaths(gTruth, pathPairs);
 
+
+
 % If unresolvedPaths is empty, then the paths were successfully updated
 if isempty(unresolvedPaths)
     % Save the updated gTruth object back to the MAT file
-    save('side1_03_new.mat', 'gTruth', '-append');
+    save('Side2_03.mat', 'gTruth', '-append');
     disp('File path updated successfully.');
 else
     disp('Some paths could not be updated. Check unresolvedPaths for details.');
